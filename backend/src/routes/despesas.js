12b2154e21016578052e1125a5bb7bc8451746c3
Matheusfonte <fontematheus454@gmail.com>
2026-07-02 18:27:29 -0300

    Final

--- backend/src/routes/despesas.js
+++ backend/src/routes/despesas.js
@@ -1,6 +1,17 @@
 const express = require('express');
 const multer = require('multer');
-const upload = multer({ dest: 'uploads/' });
+const path = require('path');
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => cb(null, 'uploads/'),
+  filename: (req, file, cb) => {
+    const ext = path.extname(file.originalname);
+    const fileName = `${Date.now()}-${Math.round(Math.random() * 1e9)}${ext}`;
+    cb(null, fileName);
+  },
+});
+
+const upload = multer({ storage });
 const { 
   listExpenses, 
   getExpenseById, 
